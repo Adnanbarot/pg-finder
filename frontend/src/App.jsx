3e430b7dba951027c5814d1fd2837463a64060a5
Adnan Barot <adnanbarot03@gmail.com>
2026-09-23 22:46:17 +0530

    Create initial React frontend and navbar

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+
+      <h1>PG Finder</h1>
+      <p>Find a place that feels like home.</p>
+    </div>
+  );
+}
+
+export default App;
